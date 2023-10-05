3451294f0b77f280db0aefd8ec8febb43303f21b
andresquipicadoo <aquirosp592@gmail.com>
2023-10-05 13:45:30 -0600

    Remover import no utilizado

--- ClimasPronosticos/src/Clima/LeerJSONCantones.java
+++ ClimasPronosticos/src/Clima/LeerJSONCantones.java
@@ -9,7 +9,6 @@ import java.net.URISyntaxException;
 import java.net.URL;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
 
